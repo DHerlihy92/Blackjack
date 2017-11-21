0ab249ea2b006a2340527caaf3fb3105084176fb
DHerlihy92 <32128694+DHerlihy92@users.noreply.github.com>
2017-11-21 12:48:22 +0000

    Update 1
    created a deckbuilder method in Deck and moved the code to make the array to this method.
    Created a method that shuffles the positions in the array in Deck and tested the result

--- src/BlackJackDriver.java
+++ src/BlackJackDriver.java
@@ -3,8 +3,11 @@ import javax.swing.*;
 public class BlackJackDriver {
     public static void main(String[] args) {
 
-        deckBuilder();
+        Card[] deck = Deck.deckBuilder();
         Deck bdeck = new Deck(deck);
         bdeck.display();
+        Deck.shuffle(deck);
+        bdeck.display();
+
     }
 }
--- src/Deck.java
+++ src/Deck.java
@@ -30,42 +30,53 @@ public class Deck {
 
         JOptionPane.showMessageDialog(null,text);
     }
-    
-    public void deckBuilder(){
+
+    public static Card[] deckBuilder() {
         Card[] deck = new Card[52];
-        int[] values = {2,3,4,5,6,7,8,9,10,10,10,10,11};
-        String[] names = {"two","three","four","five","six","seven","eight","nine","ten","jack","queen","king","ace"};
-        int y=0, count=0;
+        int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
+        String[] names = {"two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"};
+        int y = 0, count = 0;
 
-        for(int i=0;i<=51;i++){
+        for (int i = 0; i <= 51; i++) {
 
-            deck[i]=new Card();
+            deck[i] = new Card();
 
-            if(count<13){
+            if (count < 13) {
                 deck[i].setSuit("Hearts");
                 deck[i].setValue(values[y]);
                 deck[i].setName(names[y]);
             }
-            if(count>=13 && count<26){
+            if (count >= 13 && count < 26) {
                 deck[i].setSuit("Diamonds");
                 deck[i].setValue(values[y]);
                 deck[i].setName(names[y]);
             }
-            if(count>=26 && count<39){
+            if (count >= 26 && count < 39) {
                 deck[i].setSuit("Clubs");
                 deck[i].setValue(values[y]);
                 deck[i].setName(names[y]);
             }
-            if(count>=39 && count<51){
+            if (count >= 39 && count <= 51) {
                 deck[i].setSuit("Spades");
                 deck[i].setValue(values[y]);
                 deck[i].setName(names[y]);
             }
             count++;
             y++;
-            if(count==12 || count==25 || count==38){
-                y=0;
+            if (count == 13 || count == 26 || count == 39) {
+                y = 0;
             }
 
         }
-}
+        return deck;
+    }
+
+    public static void shuffle(Card[] deck) {
+        for ( int i = deck.length-1; i > 0; i-- ) {
+            int rand = (int)(Math.random()*(i+1));
+            Card temp = deck[i];
+            deck[i] = deck[rand];
+            deck[rand] = temp;
+        }
+    }
+    }
